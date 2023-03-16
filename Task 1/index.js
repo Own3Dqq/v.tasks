ad3ed27a01ec113338d9a5edba09fd8e6e110955
Own3Dqq <own3dqq@gmail.com>
2023-03-16 11:41:45 +0200

    invtoduced changes by refactoring code

--- Task 1/index.js
+++ Task 1/index.js
@@ -6,33 +6,27 @@
 import users from './data/data.json' assert { type: 'json' };
 import { convertDollarsToNumber } from './helpers/helper.js';
 
-const filterUserPhonesByBalance = (value, index) => {
-	const generateFilterArr = [];
-
-	if (Number.isInteger(index) && Array.isArray(value)) {
-		for (let i = 0; i < value.length; i++) {
-			const element = value[i];
-			if (convertDollarsToNumber(element.balance) >= index) {
-				generateFilterArr.push(element.phone);
-			}
-		}
-	} else {
-		throw 'TypeError: Arguments wrong type.';
-	}
+const filterUserPhonesByBalance = (value, minBalance) => {
+	const generateFilterArr = value
+		.filter((item) => {
+			return convertDollarsToNumber(item.balance) >= minBalance;
+			А;
+		})
+		.map((item) => {
+			return item.phone;
+		});
 
 	return generateFilterArr;
 };
 
-filterUserPhonesByBalance(users, 2000);
+console.log(filterUserPhonesByBalance(users, 2000));
 
 const getSumAllUsersBalance = (value) => {
-	let sumUsersBalance = 0;
+	const findAllUsersBalance = value.reduce((acc, currentValue) => {
+		return acc + convertDollarsToNumber(currentValue.balance);
+	}, 0);
 
-	const findAllUsersBalance = value.map((item) => {
-		return (sumUsersBalance += convertDollarsToNumber(item.balance));
-	});
-
-	return sumUsersBalance;
+	return findAllUsersBalance;
 };
 
 getSumAllUsersBalance(users);
