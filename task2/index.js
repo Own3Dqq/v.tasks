f513d8d69a7f9e60730ca902b8f8d9fca77ec3c0
Own3Dqq <own3dqq@gmail.com>
2023-03-18 19:10:59 +0200

    made function sum() which added number

--- task2/index.js
+++ task2/index.js
@@ -5,8 +5,32 @@
 */
 
 const pow = (number, degree) => {
-	if (!isNaN(number) || !isNaN(degree)) return degree == 1 ? number : number * pow(number, degree - 1);
+	if (degree > 0 && Number.isInteger(number) && Number.isInteger(degree)) {
+		return degree == 1 ? number : number * pow(number, degree - 1);
+	}
+
 	throw new TypeError('function pow() expects number as first and second argument.');
 };
 
-alert(pow('2', 5));
+console.log(pow(2, 4));
+
+/* 
+Написати функцію, яка приймає 1 параметр. і підсумовує з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+
+sum(3) = 3
+sum(5) = 8
+sum(20) = 28 
+*/
+
+function sum() {
+	let result = 0;
+	return function (value) {
+		return (result += value);
+	};
+}
+
+let addNumber = sum();
+
+console.log(addNumber(3));
+console.log(addNumber(5));
+console.log(addNumber(320));
