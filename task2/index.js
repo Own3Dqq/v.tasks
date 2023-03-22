520783a7c380d4b84b8acf93b126f768fe19242c
Own3Dqq <own3dqq@gmail.com>
2023-03-22 14:54:22 +0200

    implemented task currying function

--- task2/index.js
+++ task2/index.js
@@ -6,13 +6,15 @@
 
 const pow = (number, degree) => {
 	if (degree > 0 && Number.isInteger(number) && Number.isInteger(degree)) {
-		return degree == 1 ? number : number * pow(number, degree - 1);
+		return degree === 1 ? number : number * pow(number, degree - 1);
 	}
 
-	throw new TypeError('function pow() expects number as first and second argument.');
+	throw new TypeError(`function pow() expects number as first, and positive number as second.`);
 };
 
-console.log(pow(2, 4));
+console.log(pow(2, 1));
+console.log(pow(-5, 2));
+// console.log(pow(5, -1));
 
 /* 
 Написати функцію, яка приймає 1 параметр. і підсумовує з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
@@ -34,3 +36,26 @@ let addNumber = sum();
 console.log(addNumber(3));
 console.log(addNumber(5));
 console.log(addNumber(320));
+
+/* За допомогою замикання написати ф-цію structureUserInfo, яка збирає інфо-об'єкт про користувача з аргументів.
+
+Приклад:
+
+structureUserInfo('John')('Admin') // {role: "Admin", name: "John"} */
+
+const structUserInfo = (position) => {
+	return function (name) {
+		return function (age) {
+			return function (religion) {
+				return {
+					position,
+					name,
+					age,
+					religion,
+				};
+			};
+		};
+	};
+};
+
+console.log(structUserInfo('Admin')('Vladyslav')(27)('Atheist'));
